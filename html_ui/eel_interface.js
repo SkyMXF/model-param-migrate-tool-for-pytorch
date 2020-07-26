7a9a2304e70010233183dc1044f5ffa9ed16ab37
SkyMXF <1665567220@qq.com>
2020-07-27 01:50:05 +0800

    now it can run

--- html_ui/eel_interface.js
+++ html_ui/eel_interface.js
@@ -1,64 +1,194 @@
-const srcFileSelect = document.getElementById("src-file-select");
-const aimFileSelect = document.getElementById("aim-file-select");
+// load layui module
+layui.use(['layer', 'tree', 'util', 'table'], function(){
+    var tree = layui.tree
+    ,layer = layui.layer
+    ,util = layui.util
+    ,table = layui.table
 
-// get model list for src and aim model select elem
-function update_model_list(){
-    eel.eel_get_model_list()().then(
-        function(model_list){
-            srcFileSelect.length = 1;
-            aimFileSelect.length = 1;
-            for (var i = 0; i < model_list.length; i++){
-                srcFileSelect.options.add(new Option(model_list[i], model_list[i]));
-                aimFileSelect.options.add(new Option(model_list[i], model_list[i]));
+    // element
+    const srcFileSelect = document.getElementById("src-file-select");
+    const aimFileSelect = document.getElementById("aim-file-select");
+    const srcTreeView = document.getElementById("srctree");
+    const aimTreeView = document.getElementById("aimtree");
+    const migrateListView = document.getElementById("migrate-list");
+    const addEntryButton = document.getElementById("add-entry-button");
+    const removeEntryButton = document.getElementById("remove-entry-button");
+    const applyMigrationButton = document.getElementById("apply-migration-button");
+
+    // data
+    var nowChosenSrcNode = null;
+    var nowChosenAimNode = null;
+
+    eel.refresh_all_data()
+
+    // get model list for src and aim model select elem
+    function update_model_list(){
+        eel.eel_get_model_list()().then(
+            function(model_list){
+                srcFileSelect.length = 1;
+                aimFileSelect.length = 1;
+                for (var i = 0; i < model_list.length; i++){
+                    srcFileSelect.options.add(new Option(model_list[i], model_list[i]));
+                    aimFileSelect.options.add(new Option(model_list[i], model_list[i]));
+                }
             }
-        }
-    );
-}
-update_model_list();
+        );
+    };
+    update_model_list();
 
-// add listener for file select elem
-srcFileSelect.addEventListener("change", onChooseSrcPth);
-aimFileSelect.addEventListener("change", onChooseAimPth);
+    // add listener for file select elem
+    srcFileSelect.addEventListener("change", onChooseSrcPth);
+    aimFileSelect.addEventListener("change", onChooseAimPth);
 
-function onChooseSrcPth(){
-    eel.eel_get_tree_view(srcFileSelect.value)().then(
-        function(tree_dict){
-            tree_dict = parseTreeForView(tree_dict);
-            layui.use(['tree', 'util'], function(){
-                var tree = layui.tree
-                ,layer = layui.layer
-                ,util = layui.util
+    function onChooseSrcPth(){
+        nowChosenSrcNode = null;
+        eel.load_src_tree(srcFileSelect.value)().then(
+            function(tree_dict){
+                tree_dict = parseTreeForView(tree_dict);
 
                 tree.render({
                     elem: '#srctree'
                     ,data: tree_dict
-                    ,showCheckbox: true
+                    ,onlyIconControl: true
+                    ,click: function(chosenObj){
+                        if (nowChosenSrcNode !== null){
+                            nowChosenSrcNode.elem.removeClass("layui-bg-grass");
+                        }
+                        nowChosenSrcNode = chosenObj;
+                        nowChosenSrcNode.elem.addClass("layui-bg-grass");
+                    }
                 });
-            });
-        }
-    );
-};
+            }
+        );
+    };
 
-function onChooseAimPth(){
-    eel.eel_get_tree_view(aimFileSelect.value)().then(
-        function(tree_dict){
-            tree_dict = parseTreeForView(tree_dict);
-            layui.use(['tree', 'util'], function(){
-                var tree = layui.tree
-                ,layer = layui.layer
-                ,util = layui.util
+    function highlightAllChildren(node){
+
+    };
+    function lowlightAllChildren(node){
+
+    };
+
+    function onChooseAimPth(){
+        nowChosenAimNode = null;
+        eel.load_aim_tree(aimFileSelect.value)().then(
+            function(tree_dict){
+                tree_dict = parseTreeForView(tree_dict);
 
                 tree.render({
                     elem: '#aimtree'
                     ,data: tree_dict
-                    ,showCheckbox: true
+                    ,onlyIconControl: true
+                    ,click: function(chosenObj){
+                        if (nowChosenAimNode !== null){
+                            nowChosenAimNode.elem.removeClass("layui-bg-skyblue");
+                        }
+                        nowChosenAimNode = chosenObj;
+                        nowChosenAimNode.elem.addClass("layui-bg-skyblue");
+                    }
                 });
-            });
-        }
-    );
-};
+            }
+        );
+    };
 
-// function for tree view
-function parseTreeForView(tree_dict){
-    return tree_dict;
-};
+    // function for tree view
+    function parseTreeForView(tree_dict){
+        return tree_dict;
+    };
+
+    // migrate list view
+    function updateMigrateListView(migrate_list){
+        
+        table.render({
+            elem: '#migrate-list'
+            ,data: migrate_list
+            ,cols: [[
+                {field:"choose", title:"", type:'checkbox', width:"10%", fixed:"left"}
+                ,{field:'src', width:"35%", title: 'src', sort: true}
+                ,{field:'aim', width:"35%", title: 'aim', sort: true}
+                ,{field:'shape', width: "30%", title: 'shape'}
+            ]]
+        });
+    };
+    updateMigrateListView([]);
+
+    // button event
+    function addMigrationEntry(){
+        if ((nowChosenSrcNode === null) || (nowChosenSrcNode === null)){
+            layer.msg("Operation FAILED: Please choose nodes in src and aim params tree first.", {
+                time: 10000, // closed after n ms
+                btn: ['OK']
+            });
+            return;
+        };
+        eel.add_migration_entry(nowChosenSrcNode.data, nowChosenAimNode.data)().then(
+            function(data){
+                // data: [message, migrate_list]
+                message = data[0];
+                if (message.length == 0){
+                    // if message is empty, operation succeed
+                    migrate_list = data[1];
+                    updateMigrateListView(migrate_list);
+                }
+                else{
+                    layer.msg(message, {
+                        time: 20000, // closed after n ms
+                        btn: ['OK']
+                    });
+                };
+            }
+        );
+    };
+    addEntryButton.addEventListener("click", addMigrationEntry);
+
+    function removeMigrationEntry(){
+        var listLines = table.cache["migrate-list"];
+        var checkedIdxList = [];
+        for (var i = 0; i < listLines.length; i++){
+            if (listLines[i]["LAY_CHECKED"]){
+                checkedIdxList.push(i);
+            };
+        };
+        eel.remove_migration_entry(checkedIdxList)().then(
+            function(data){
+                // data: [message, migrate_list]
+                message = data[0];
+                if (message.length == 0){
+                    // if message is empty, operation succeed
+                    migrate_list = data[1];
+                    updateMigrateListView(migrate_list);
+                }
+                else{
+                    layer.msg(message, {
+                        time: 20000, // closed after n ms
+                        btn: ['OK']
+                    });
+                };
+            }
+        );
+    };
+    removeEntryButton.addEventListener("click", removeMigrationEntry);
+
+    function applyMigration(){
+        eel.apply_migration()().then(
+            function(data){
+                // data: [message, migrate_list]
+                message = data[0];
+                if (message.length == 0){
+                    // if message is empty, operation succeed
+                    migrate_list = data[1];
+                }
+                else{
+                    layer.msg(message, {
+                        time: 20000, // closed after n ms
+                        btn: ['OK']
+                    });
+                };
+                updateMigrateListView(migrate_list);
+            }
+        );
+    };
+    applyMigrationButton.addEventListener("click", applyMigration);
+
+});
+
--- param_migrate.py
+++ param_migrate.py
@@ -4,44 +4,100 @@ import glob
 import torch
 import eel
 
-LEAF_NODE_KEY = "PT_LEAF"
 MODEL_DIR = os.path.join("..", "pretrained")
+src_params_tree = None
+aim_params_tree = None
+migration_entry_list = []
+migration_view_list = []
+id_for_tree_node = 0
+src_model_path = None
+aim_model_path = None
 
 def load_model(model_path):
     return torch.load(model_path)
 
+def save_model(model, model_path):
+    torch.save(model, model_path)
+
 def get_params_tree(model_dict):
     """return a dict like:
-    {
-        "encoder": {
-            "layer0": {
-                "conv0": {...}
-            },
-            "layer1": {...}
+    [
+        {
+            "name": "encoder",
+            "id": 0,
+            "leaf": False,
+            "shape": None,
+            "father": None,
+            "children": [
+                "name": "conv1",
+                "id": 1,
+                "leaf": False,
+                "shape": None,
+                "father": ...,  # pointer to father node dict
+                "children": [...]
+            ]
         },
-        "decoder": {...}
-    }
-    - Leaf node will have only one key named by LEAF_NODE_KEY, and its value is its shape(type: str)
+        {...},
+        ...
+    ]
+    - one dict for every node, list means edges of tree
     """
-    tree_dict = {}
+    tree_list = []
+    node_id = 0
     for arg_name in model_dict.keys():
         # arg_name form like "encoder.layer0.conv0.weights"
-        levels = str(arg_name).split(".")
-        now_level_dict = tree_dict
-        for level_name in levels:
-            # enter or build tree level until leaf node
-            if level_name in now_level_dict.keys():
-                # find this level, enter
-                now_level_dict = now_level_dict[level_name]
+        nodes = str(arg_name).split(".")
+        now_node_list = tree_list
+        now_father_node = None
+        for depth_idx in range(len(nodes)):
+            node_name = nodes[depth_idx]
+            # enter or build tree node until leaf node
+            aim_node_idx = -1
+            for node_idx in range(len(now_node_list)):
+                # find aim node dict
+                if now_node_list[node_idx]["name"] == node_name:
+                    aim_node_idx = node_idx
+                    break
+            if aim_node_idx < 0:
+                # did not find node, build it
+                now_node_list.append({
+                    "name": node_name,
+                    "id": node_id,
+                    "leaf": False,
+                    "shape": None,
+                    "father": now_father_node,
+                    "children": []
+                })
+                node_id += 1
+                aim_node_idx = len(now_node_list) - 1
+                
+            if depth_idx == len(nodes) - 1:
+                # leaf node
+                now_node_list[aim_node_idx]["leaf"] = True
+                now_node_list[aim_node_idx]["shape"] = model_dict[arg_name].shape
             else:
-                # did not find this level, build it and then enter
-                now_level_dict[level_name] = {}
-                now_level_dict = now_level_dict[level_name]
-        
-        # get tensor shape for leaf node
-        now_level_dict[LEAF_NODE_KEY] = str(list(model_dict[arg_name].shape))
+                # branch node, enter
+                now_father_node = now_node_list[aim_node_idx]
+                now_node_list = now_node_list[aim_node_idx]["children"]
     
-    return tree_dict
+    return tree_list
+
+def get_root_node(tree_list:list):
+    """list of tree -> an abstract root node(dict)
+    """
+    return {
+        "name": "",
+        "id": -1,
+        "leaf": False,
+        "shape": None,
+        "father": None,
+        "children": tree_list
+    }
+
+def print_tree(tree_list, space_num=0):
+    for node in tree_list:
+        print(space_num * " " + "-%s, %d, isLeaf:%s, father:%s"%(node["name"], node["id"], str(node["leaf"]), node["father"]["name"]))
+        print_tree(node["children"], space_num+1)
 
 @eel.expose
 def eel_get_model_list():
@@ -50,32 +106,168 @@ def eel_get_model_list():
         model_path_list[model_idx] = os.path.basename(model_path_list[model_idx])
     return model_path_list
 
-id_for_tree_node = 0
 @eel.expose
-def eel_get_tree_view(model_name):
-    model_path = os.path.join(MODEL_DIR, model_name)
-    tree_dict = get_params_tree(load_model(model_path))
-
+def load_src_tree(model_name):
+    global src_params_tree, src_model_path
+    src_model_path = os.path.join(MODEL_DIR, model_name)
+    src_params_tree = get_params_tree(load_model(src_model_path))
+    
     # convert to layui tree struct
     global id_for_tree_node
     id_for_tree_node = 0
     list_for_view = []
-    parse_dict_tree(tree_dict, list_for_view)
+    parse_tree_for_view(src_params_tree, list_for_view)
 
     return list_for_view
 
-def parse_dict_tree(now_dict:dict, now_list:list, upper_list=None):
+@eel.expose
+def load_aim_tree(model_name):
+    global aim_params_tree, aim_model_path
+    aim_model_path = os.path.join(MODEL_DIR, model_name)
+    aim_params_tree = get_params_tree(load_model(aim_model_path))
+    
+    # convert to layui tree struct
     global id_for_tree_node
-    for k in now_dict.keys():
-        if k == LEAF_NODE_KEY:
-            # reach leaf node, add shape info to title
-            upper_list[-1]["title"] += now_dict[LEAF_NODE_KEY]
-            break
-        now_list.append(
-            {"title": k, "id": id_for_tree_node, "children": []}
-        )
+    id_for_tree_node = 0
+    list_for_view = []
+    parse_tree_for_view(aim_params_tree, list_for_view)
+
+    return list_for_view
+
+def parse_tree_for_view(now_node_list:list, now_view_list:list):
+    global id_for_tree_node
+    for node in now_node_list:
+        now_view_list.append({"title": node["name"], "id": id_for_tree_node, "children": []})
         id_for_tree_node += 1
-        parse_dict_tree(now_dict[k], now_list=now_list[-1]["children"], upper_list=now_list)
+        if node["leaf"]:
+            # leaf node
+            now_view_list[-1]["title"] += str(list(node["shape"]))
+        else:
+            # branch node
+            parse_tree_for_view(node["children"], now_view_list[-1]["children"])
+
+# migration part
+@eel.expose
+def add_migration_entry(src_sub_view_node:dict, aim_sub_view_node:dict):
+
+    global src_params_tree, aim_params_tree
+    
+    # find sub node in real tree struct
+    src_sub_real_node = find_real_node(src_sub_view_node["id"], src_params_tree)
+    aim_sub_real_node = find_real_node(aim_sub_view_node["id"], aim_params_tree)
+
+    # find all matched leaf node
+    matched_leafs = []
+    find_match_node(src_sub_real_node, aim_sub_real_node, matched_leafs)
+    if len(matched_leafs) <= 0:
+        return ["Operation FAILED: Could not find any matched nodes.\nHINT: Only those nodes with the same shape(or sub nodes with the same name and shape) will be matched.", migration_view_list]
+
+    # add into migrate list
+    for match_tuple in matched_leafs:
+        # get node path in the whole tree
+        src_node_path, src_node_str_path = get_node_path(match_tuple[0])
+        aim_node_path, aim_node_str_path = get_node_path(match_tuple[1])
+
+        # append
+        migration_entry_list.append({"src": match_tuple[0], "src_path": src_node_str_path, "aim": match_tuple[1], "aim_path": aim_node_str_path})
+        migration_view_list.append({"src": src_node_str_path, "aim": aim_node_str_path, "shape": str(list(match_tuple[0]["shape"]))})
+
+    return ["", migration_view_list]
+
+def find_real_node(view_node_id: int, now_real_tree:list):
+    for node_idx_in_layer in range(len(now_real_tree)):
+        if now_real_tree[node_idx_in_layer]["id"] == view_node_id:
+            # successfully find real node
+            return now_real_tree[node_idx_in_layer]
+        elif now_real_tree[node_idx_in_layer]["id"] > view_node_id:
+            # try to find in brother node
+            return find_real_node(view_node_id, now_real_tree[node_idx_in_layer - 1]["children"])
+    # try to find in last node
+    return find_real_node(view_node_id, now_real_tree[-1]["children"])
+
+def find_match_node(src_node:dict, aim_node:dict, match_leaf_list:list):
+    if src_node["leaf"] and aim_node["leaf"]:
+        # reach leaf node, compare their name and shape
+        if src_node["name"] == aim_node["name"] and src_node["shape"] == aim_node["shape"]:
+            match_leaf_list.append((src_node, aim_node))
+    elif (not src_node["leaf"]) and (not aim_node["leaf"]):
+        # both branch node in src and aim
+        for src_sub_node in src_node["children"]:
+            for aim_sub_node in aim_node["children"]:
+                # try to find deeper match nodes if nodes have the same name in src and aim
+                if src_sub_node["name"] == aim_sub_node["name"]:
+                    find_match_node(src_sub_node, aim_sub_node, match_leaf_list)
+
+def get_node_path(node:dict):
+    now_node = node
+    path_list = [node]
+    str_path = node["name"]
+    while now_node["father"]:
+        now_node = now_node["father"]
+        path_list.append(now_node)
+        str_path = now_node["name"] + "." + str_path
+    
+    return path_list.reverse(), str_path
+
+@eel.expose
+def remove_migration_entry(rm_entry_idxs:list):
+    rm_entry_idxs.sort()
+    for rm_num in range(len(rm_entry_idxs)):
+        migration_entry_list.pop(rm_entry_idxs[rm_num] - rm_num)
+        migration_view_list.pop(rm_entry_idxs[rm_num] - rm_num)
+    
+    return ["", migration_view_list]
+
+@eel.expose
+def apply_migration():
+    # load model
+    global src_model_path, aim_model_path
+    src_model_dict = load_model(src_model_path)
+    aim_model_dict = load_model(aim_model_path)
+
+    message = ""
+    
+    # apply migration
+    global migration_entry_list, migration_view_list
+    entry_copy = migration_entry_list[:]
+    view_copy = migration_view_list[:]
+    """
+    for migrate_entry in migration_entry_list:
+        apply_entry(src_model_dict, aim_model_dict, migrate_entry)
+    save_model(aim_model_dict, aim_model_path + ".migrated")
+    migration_entry_list = []
+    migration_view_list = []
+    return ["", migration_view_list]
+    """
+    try:
+        entry_idx = 0
+        for migrate_entry in migration_entry_list:
+            apply_entry(src_model_dict, aim_model_dict, migrate_entry)
+            entry_idx += 1
+        save_model(aim_model_dict, aim_model_path + ".migrated")
+        migration_entry_list = []
+        migration_view_list = []
+        message = "Migration succeeded. Saved new model params to %s, remember to remove the '.migrated' in the file name before use it."%(aim_model_path + ".migrated")
+    except Exception as e:
+        message = "Migration FAILED: catch an error when applying entry %s->%s"%(migration_entry_list[entry_idx]["src_path"], migration_entry_list[entry_idx]["aim_path"]) + str(e)
+        # restore migration entry
+        migration_entry_list = entry_copy
+        migration_view_list = view_copy
+    finally:
+        return [message, migration_view_list]
+
+def apply_entry(src_model_dict, aim_model_dict, migrate_entry):
+    aim_model_dict[migrate_entry["aim_path"]] = src_model_dict[migrate_entry["src_path"]]
+
+@eel.expose
+def refresh_all_data():
+    global migration_entry_list, migration_view_list, src_params_tree, aim_params_tree, src_model_path, aim_model_path
+    migration_entry_list = []
+    migration_view_list = []
+    src_params_tree = None
+    aim_params_tree = None
+    src_model_path = None
+    aim_model_path = None
 
 def main():
     eel.init("html_ui")
@@ -84,6 +276,7 @@ def main():
         "port": 8000,
         "chromeFlags": ["-kiosk"]
     }
+    print("Start a server on http://localhost:8000/main.html ...")
     eel.start("main.html", options=options, suppress_error=True)
 
 if __name__ == "__main__":
